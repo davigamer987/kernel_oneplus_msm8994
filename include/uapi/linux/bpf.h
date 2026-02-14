dcaa9a1a4039dec47feb3724613cb0716316109a
Yaroslav Zviezda <acroreiser@gmail.com>
2026-02-14 16:45:46 +0200

    fixup! apply missing part of https://github.com/acroreiser/android_kernel_lenovo_a6010/commit/d7c48eac79d746176dcfd6f884b4cb763b9ebd3c

--- include/uapi/linux/bpf.h
+++ include/uapi/linux/bpf.h
@@ -217,6 +217,55 @@ union bpf_attr {
 		__u32		attach_type;
 		__u32		attach_flags;
 	};
+
+	struct { /* anonymous struct used by BPF_PROG_TEST_RUN command */
+		__u32           prog_fd;
+		__u32           retval;
+		__u32           data_size_in;
+		__u32           data_size_out;
+		__aligned_u64   data_in;
+		__aligned_u64   data_out;
+		__u32           repeat;
+		__u32           duration;
+	} test;
+
+	struct { /* anonymous struct used by BPF_*_GET_*_ID */
+		union {
+			__u32		start_id;
+			__u32		prog_id;
+			__u32		map_id;
+		};
+		__u32		next_id;
+		__u32		open_flags;
+	};
+
+	struct { /* anonymous struct used by BPF_OBJ_GET_INFO_BY_FD */
+		__u32		bpf_fd;
+		__u32		info_len;
+		__aligned_u64	info;
+	} info;
+
+	struct { /* anonymous struct used by BPF_PROG_QUERY command */
+		__u32		target_fd;	/* container object to query */
+		__u32		attach_type;
+		__u32		query_flags;
+		__u32		attach_flags;
+		__aligned_u64	prog_ids;
+		__u32		prog_cnt;
+	} query;
+
+	struct { /* anonymous struct used by BPF_RAW_TRACEPOINT_OPEN command */
+		__u64 name;
+		__u32 prog_fd;
+	} raw_tracepoint;
+
+	struct { /* anonymous struct for BPF_BTF_LOAD */
+		__aligned_u64	btf;
+		__aligned_u64	btf_log_buf;
+		__u32		btf_size;
+		__u32		btf_log_size;
+		__u32		btf_log_level;
+	};
 } __attribute__((aligned(8)));
 
 /* integer value in 'imm' field of BPF_CALL instruction selects which helper
