f6685d1dcdf204e852feddfde169929a2ec9d08b
Davi <davimarchesiprimo@gmail.com>
2026-06-25 05:17:57 -0300

    bpf: add check_map_access_adj for adjusted pointer bounds checking
    Add check_map_access_adj() to validate accesses via PTR_TO_MAP_VALUE_ADJ
    pointers, checking both min and max tracked values against the map value
    size to prevent out-of-bounds access on adjusted map value pointers.

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -706,6 +706,35 @@ static int check_map_access(struct bpf_verifier_env *env, u32 regno, int off,
 	return 0;
 }
 
+static int check_map_access_adj(struct bpf_verifier_env *env, u32 regno,
+				int off, int size)
+{
+	struct bpf_verifier_state *state = &env->cur_state;
+	struct bpf_reg_state *reg = &state->regs[regno];
+	int err;
+
+	if (log_level)
+		print_verifier_state(state);
+	env->varlen_map_value_access = true;
+	if (reg->min_value < 0) {
+		verbose("R%d min value is negative, either use unsigned index or do a if (index >=0) check.\n",
+			regno);
+		return -EACCES;
+	}
+	err = check_map_access(env, regno, reg->min_value + off, size);
+	if (err) {
+		verbose("R%d min value is outside of the array range\n",
+			regno);
+		return err;
+	}
+	if (reg->max_value == BPF_REGISTER_MAX_RANGE) {
+		verbose("R%d unbounded memory access, make sure to bounds check any array access into a map\n",
+			regno);
+		return -EACCES;
+	}
+	return check_map_access(env, regno, reg->max_value + off, size);
+}
+
 #define MAX_PACKET_OFF 0xffff
 
 static bool may_access_direct_pkt_data(struct bpf_verifier_env *env,
